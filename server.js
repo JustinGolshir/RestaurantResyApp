6d6cf15d2b416822bfdd8003b94d20dbcf20e83e
justin <justin@tcadmins-MacBook-Pro-59.local>
2018-04-12 13:30:05 -0400

    First Update

---
+++ server.js
@@ -0,0 +1,75 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// New Reservation Info
+// =============================================================
+var newReservation = [
+  {
+    customerName: '#reserve_name',
+    phoneNumber: '#reserve_phone',
+    customerEmail: '#reserve_email',
+    customerID: '#reserve_uniqueID',
+  },
+];
+
+// Routes
+// =============================================================
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/api/tables", function (req, res) {
+  return res.json(characters);
+});
+
+app.get("/api/tables/:tables", function (req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (var i = 0; i < tables.length; i++) {
+    if (chosen === tables[i].routeName) {
+      return res.json(tables[i]);
+    }
+  }
+
+  return res.json(false);
+
+});
+
+app.post("/api/tables", function (req, res) {
+  var newReservation = req.body;
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  characters.push(newReservation);
+
+  res.json(newReservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
